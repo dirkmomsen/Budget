0e1a585bfaeea0e22bdae7d5ac3679ea56f154f9
dirkmomsen <dirkmomsen@hotmail.com>
2020-11-28 18:08:04 +1300

    Added dotnet identity wit users and roles

---
+++ API/Data/Seed.cs
@@ -0,0 +1,27 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(UserManager<AppUser> userManager)
+        {
+            if (await userManager.Users.AnyAsync()) return;
+
+            var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            foreach (var user in users)
+            {
+                user.UserName = user.UserName.ToLower();
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+            }
+        }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
 namespace API.Entities
 {
-    public class AppUser
+    public class AppUser : IdentityUser<int>
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        
+        public ICollection<AppUserRole> UserRoles { get; set; }
     }
 }
